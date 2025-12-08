3f532327dd62f5908a1aa7f21280705efef8b827
soniamereva-eng <soniamereva@gmail.com>
2025-12-08 12:36:30 +0100

    Add Admin component and conditional rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // <-- important : import Tailwind
+import Admin from "./Admin.jsx";
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {path === "/admin" ? <Admin /> : <App />}
   </React.StrictMode>
 );
